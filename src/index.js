b57b2f20d25b48813c267bb7380f20ac083e2c50
Gant <GantMan@gmail.com>
2017-09-30 10:49:18 -0500

    attempt help

--- src/index.js
+++ src/index.js
@@ -7,28 +7,10 @@ const { BRAND, configureRuntime } = require('./runtime')
  * Runs solidarity.
  */
 module.exports = async function run (argv) {
-  let context // the gluegun execution environment for a command
-  let runtime // the gluegun runtime which will execute a command
-
-  // Attempt to setup the runtime.
-  // try {
   // setup the runtime
   build()
-    // Brand is used for default config files (if any).
     .brand('solidarity')
-    // The default plugin is the directory we're in right now, so
-    // the commands sub-directory will contain the first right of
-    // refusal to handle user's requests.
     .src(__dirname)
-    // TODO: maybe there's other places you'd like to load plugins from?
-    // .load(`~/.${BRAND}`)
-
-    // These are the magic tokens found inside command js sources
-    // which plugin authors use to specify the command users can type
-    // as well as the help they see.
-    // .token('commandName', `${BRAND}Command`)
-    // .token('commandDescription', `${BRAND}Description`)
-    // let's build it
     .create()
     .run()
 
