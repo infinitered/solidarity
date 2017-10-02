84e98c9d95f26ac29cb929f795f2b97cbc8a7078
Gant <GantMan@gmail.com>
2017-10-01 23:51:22 -0500

    stub logic

--- src/commands/snapshot.js
+++ src/commands/snapshot.js
@@ -1,7 +1,24 @@
 const run = async function (context) {
+  const { print, prompt, filesystem } = context
   // check is there an existing .solidarity file?
-  // if yes, load in existing file checks and update them
-  // if no, prompt for environment packages
+  if (filesystem.exists('.solidarity')) {
+    // load existing file and update rule versions
+    print.info('Now loading latest environment')
+  } else {
+    // prompt for environment packages
+    const userAnswer = await prompt.ask({
+      name: 'createFile',
+      type: 'confirm',
+      message: 'No `.solidarity` file found for this project.  Would you like to create one?'
+    })
+
+    if (userAnswer.createFile) {
+      // list visible plugins
+      print.info('Available technology plugins')
+    } else {
+      print.info('Nothing to do')
+    }
+  }
 }
 
 module.exports = {
