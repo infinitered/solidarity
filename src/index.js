ea060b9cd3dfefc56e6a00c1b52104f46a54fdea
Gant <GantMan@gmail.com>
2017-10-04 00:26:35 -0500

    spiffy up a few things

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ module.exports = async () => {
   build()
     .brand('solidarity')
     .src(__dirname)
+    // It might also be worth checking global
+    // `npm config get prefix` returns '/usr/local'
+    // global npm is stored /usr/local/lib/node_modules
+    .plugins('./node_modules', { matching: 'solidarity-*', hidden: true })
+    // for testing
+    .plugins('/Users/gantman/Documents/Projects/rn/node_packages/', { matching: 'solidarity-react-*', hidden: true })
+    .plugins('..', { matching: 'solidarity-react-*', hidden: true })
     .create()
     .run()
 }
