c0bcf77dfe42310d1a1bdfc2c01e18715229dd61
Gant <GantMan@gmail.com>
2017-10-03 23:00:46 -0500

    check for plugins and react

--- src/commands/snapshot.js
+++ src/commands/snapshot.js
@@ -1,6 +1,9 @@
 const run = async function (context) {
   const { print, prompt, filesystem } = context
-  // check is there an existing .solidarity file?
+  const printSeparator = () =>
+    print.info(print.colors.america('-----------------------------------------------------------------------------------'))
+
+    // check is there an existing .solidarity file?
   if (filesystem.exists('.solidarity')) {
     // load existing file and update rule versions
     print.info('Now loading latest environment')
@@ -14,7 +17,24 @@ const run = async function (context) {
 
     if (userAnswer.createFile) {
       // list visible plugins
-      print.info('Available technology plugins')
+      print.info('Available technology plugins:')
+      printSeparator()
+      if (context.pluginsList) {
+        print.info(context.pluginsList)
+      } else {
+        print.error(`No solidarity plugins found!
+
+        Add a plugin for a given technology:
+        ${print.colors.blue('https://github.com/infinitered/solidarity/blob/master/docs/pluginsList.md')}
+
+        OR write your own plugin for generating rules:
+        ${print.colors.blue('https://github.com/infinitered/solidarity/blob/master/docs/plugins.md')}
+
+        OR simply create a .solidarity rule-set by hand for this project:
+        ${print.colors.blue('https://github.com/infinitered/solidarity/blob/master/docs/options.md')}
+        `)
+      }
+      printSeparator()
     } else {
       print.info('Nothing to do')
     }
