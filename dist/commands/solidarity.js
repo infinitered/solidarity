421de1371c8ff9a23ce59cba7febac5dd6432ad0
Matthew Thompson <matt.thompson@salesloft.com>
2017-10-10 10:40:59 -0400

    Move solidarity.js to solidarity.ts

--- dist/commands/solidarity.js
+++ dist/commands/solidarity.js
@@ -6,50 +6,53 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const { map, toPairs, isEmpty, flatten } = require('ramda');
-const checkForEscapeHatchFlags = (context) => __awaiter(this, void 0, void 0, function* () {
-    const { print, parameters } = context;
-    const { options } = parameters;
-    if (options.help || options.h) {
-        // Just looking for help
-        print.printCommands(context);
-        process.exit(0);
-    }
-    else if (options.version || options.v) {
-        // Just looking for version
-        print.info(require('../../package.json').version);
-        process.exit(0);
-    }
-});
-const run = (context) => __awaiter(this, void 0, void 0, function* () {
-    // drop out fast in these situations
-    checkForEscapeHatchFlags(context);
-    const { print, solidarity } = context;
-    const { checkRequirement, getSolidaritySettings } = solidarity;
-    // get settings
-    const solidaritySettings = getSolidaritySettings(context);
-    // build map of checks to perform
-    const checks = yield map((requirement) => __awaiter(this, void 0, void 0, function* () { return checkRequirement(requirement, context); }), toPairs(solidaritySettings));
-    // run the array of promises you just created
-    yield Promise.all(checks)
-        .then(results => {
-        const errors = flatten(results);
-        if (isEmpty(errors)) {
-            print.success('\n Environment Checks Valid');
+var Solidarity;
+(function (Solidarity) {
+    const { map, toPairs, isEmpty, flatten } = require('ramda');
+    const checkForEscapeHatchFlags = (context) => __awaiter(this, void 0, void 0, function* () {
+        const { print, parameters } = context;
+        const { options } = parameters;
+        if (options.help || options.h) {
+            // Just looking for help
+            print.printCommands(context);
+            process.exit(0);
         }
-        else {
-            print.error('\n Solidarity Check Failed:');
-            print.error(errors);
-            process.exit(1);
+        else if (options.version || options.v) {
+            // Just looking for version
+            print.info(require('../../package.json').version);
+            process.exit(0);
         }
-    })
-        .catch(err => {
-        print.error(err);
-        process.exit(2);
     });
-});
+    Solidarity.run = (context) => __awaiter(this, void 0, void 0, function* () {
+        // drop out fast in these situations
+        checkForEscapeHatchFlags(context);
+        const { print, solidarity } = context;
+        const { checkRequirement, getSolidaritySettings } = solidarity;
+        // get settings
+        const solidaritySettings = getSolidaritySettings(context);
+        // build map of checks to perform
+        const checks = yield map((requirement) => __awaiter(this, void 0, void 0, function* () { return checkRequirement(requirement, context); }), toPairs(solidaritySettings));
+        // run the array of promises you just created
+        yield Promise.all(checks)
+            .then(results => {
+            const errors = flatten(results);
+            if (isEmpty(errors)) {
+                print.success('\n Environment Checks Valid');
+            }
+            else {
+                print.error('\n Solidarity Check Failed:');
+                print.error(errors);
+                process.exit(1);
+            }
+        })
+            .catch(err => {
+            print.error(err);
+            process.exit(2);
+        });
+    });
+})(Solidarity || (Solidarity = {}));
 // Export command
 module.exports = {
     description: 'Check environment rules',
-    run
+    run: Solidarity.run
 };
---
+++ src/commands/solidarity.ts
@@ -0,0 +1,57 @@
+namespace Solidarity {
+  const { map, toPairs, isEmpty, flatten } = require('ramda')
+  
+  const checkForEscapeHatchFlags = async(context) => {
+    const { print, parameters } = context
+    const { options } = parameters
+    if (options.help || options.h) {
+      // Just looking for help
+      print.printCommands(context)
+      process.exit(0)
+    } else if (options.version || options.v) {
+      // Just looking for version
+      print.info(require('../../package.json').version)
+      process.exit(0)
+    }
+  }
+  
+  export const run = async (context) => {
+    // drop out fast in these situations
+    checkForEscapeHatchFlags(context)
+  
+    const { print, solidarity } = context
+    const { checkRequirement, getSolidaritySettings } = solidarity
+  
+    // get settings
+    const solidaritySettings = getSolidaritySettings(context)
+  
+    // build map of checks to perform
+    const checks = await map(
+      async requirement => checkRequirement(requirement, context),
+      toPairs(solidaritySettings)
+    )
+  
+    // run the array of promises you just created
+    await Promise.all(checks)
+      .then(results => {
+        const errors = flatten(results)
+        if (isEmpty(errors)) {
+          print.success('\n Environment Checks Valid')
+        } else {
+          print.error('\n Solidarity Check Failed:')
+          print.error(errors)
+          process.exit(1)
+        }
+      })
+      .catch(err => {
+        print.error(err)
+        process.exit(2)
+      })
+  }
+}
+
+// Export command
+module.exports = {
+  description: 'Check environment rules',
+  run: Solidarity.run
+}
