4e0bb8c86d9e4f384183c17b5a849d9154198b40
Matthew Thompson <matt.thompson@salesloft.com>
2017-10-12 20:46:12 -0400

    Adds another test case for checkCLI.js

--- tests/command_helpers/checkCLI.js
+++ tests/command_helpers/checkCLI.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import checkCLI from '../../dist/extensions/functions/checkCLI'
+import solidarityExtension from '../../dist/extensions/solidarity-extension'
 
 const doesNotExistCLI = {
   binary: 'no_way_this_should_be_real'
@@ -7,6 +8,11 @@ const doesNotExistCLI = {
 
 const alwaysExistCLI = {
   binary: 'node'
+}
+
+const outOfDateCLI = {
+  binary: 'node',
+  semver: '10.99'
 }
 
 const context = require('gluegun')
@@ -17,4 +23,14 @@ test('error on missin binary', async t => {
 
 test('fine on existing binary', async t => {
   t.is(await checkCLI(alwaysExistCLI, context), undefined)
+})
+
+test('returns message on improper version', async t => {
+  solidarityExtension(context)
+  context.solidarity.getVersion = () => '1'
+  const message = `This system has an improper version for ${outOfDateCLI.binary}:
+        Rule='${outOfDateCLI.semver}'
+        Actual='1'`
+
+  t.is(await checkCLI(outOfDateCLI, context), message)  
 })
