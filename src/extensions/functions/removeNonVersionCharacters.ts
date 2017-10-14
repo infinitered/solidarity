703d5a16e758aef90beea3dca426ac1ae427b695
Gant <GantMan@gmail.com>
2017-10-14 11:45:38 -0500

    cleanup

--- src/extensions/functions/removeNonVersionCharacters.ts
+++ src/extensions/functions/removeNonVersionCharacters.ts
@@ -1,12 +1,9 @@
 module.exports = (rule, line) => {
   const foundVersions = line.match(/(\d+\.)?(\d+\.)?(\d+)([^\sa-zA-Z0-9]+\w+)?/g)
-  // Return longest match, because it is most likely to be correct
-  let result
   try {
     const matchIndex = rule.matchIndex || 0
-    result = foundVersions[matchIndex]
+    return foundVersions[matchIndex]
   } catch (_e) {
     throw ` No version was detected from the output of the binary '${rule.binary}'`
   }
-  return result
 }
