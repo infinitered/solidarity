1a8dd73ce8fd5a189bb45aa3433f0e71963672f3
Gant <GantMan@gmail.com>
2017-10-15 00:20:00 -0500

    environment tests durability

--- tests/command_helpers/checkENV.js
+++ tests/command_helpers/checkENV.js
@@ -3,9 +3,16 @@ import checkENV from '../../dist/extensions/functions/checkENV'
 
 test('checkENV detects set ENV', async t => {
   // get the first environment variable
-  const someRealEnvVar = Object.keys(process.env)[0]
-  // Use checkENV to make sure it exists
-  t.truthy(await checkENV({variable: someRealEnvVar}))
+  const environmentKeys = Object.keys(process.env)
+  if (environmentKeys.length > 0) {
+    const someRealEnvVar = environmentKeys[0]
+    // Use checkENV to make sure it exists
+    t.truthy(await checkENV({variable: someRealEnvVar}))
+  } else {
+    // This only happens in sub-run tests
+    // don't worry when this happens
+    t.pass()
+  }
 })
 
 test('checkENV can fail', async t => {
