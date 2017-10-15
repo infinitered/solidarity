14ef075e4451a30a5d9c1c86a279795a8a9ec333
Gant <GantMan@gmail.com>
2017-10-15 00:22:36 -0500

    strengthen environment test

--- tests/command_helpers/checkENV.js
+++ tests/command_helpers/checkENV.js
@@ -5,7 +5,15 @@ test('checkENV detects set ENV', async t => {
   // get the first environment variable
   const environmentKeys = Object.keys(process.env)
   if (environmentKeys.length > 0) {
-    const someRealEnvVar = environmentKeys[0]
+    let someRealEnvVar
+    let i = 0
+    // some environment vars aren't truthy
+    while (someRealEnvVar === undefined) {
+      if (process.env[environmentKeys[i]]) {
+        someRealEnvVar = environmentKeys[i]
+      }
+      i++
+    }
     // Use checkENV to make sure it exists
     t.truthy(await checkENV({variable: someRealEnvVar}))
   } else {
