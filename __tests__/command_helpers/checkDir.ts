932ca353f8e3637e37565b853801c72a26bf564d
Gant Laborde <GantMan@gmail.com>
2017-10-18 21:12:05 -0500

    clean up and make tests strong

--- __tests__/command_helpers/checkDir.ts
+++ __tests__/command_helpers/checkDir.ts
@@ -1,19 +1,19 @@
 const checkDir = require('../../src/extensions/functions/checkDir')
 const context = require('gluegun')
 
-test('checkDir detects an existing dir', async () => {
-  // Check for a known directory
-  const location = './src'
+test('checkDir detects an existing dir', () => {
+  // Check for a known directory (must be absolute)
+  const location = __dirname + '/../../src'
   // Use checkDir to make sure it exists
   expect(checkDir({location}, context)).toBeTruthy()
 })
 
-test('checkDir can fail', async () => {
+test('checkDir can fail', () => {
   // Use checkDir to make sure a non-existant directory returns false
   expect(checkDir({location: 'DOES_NOT_EXIST'}, context)).toBeFalsy()
 })
 
-test('checkDir returns false for a file that exists', async () => {
+test('checkDir returns false for a file that exists', () => {
   // Use checkDir to make sure a known file returns false since it's not a directory
   expect(checkDir({location: './package.json'}, context)).toBeFalsy()
 })
--- __tests__/command_helpers/checkFile.ts
+++ __tests__/command_helpers/checkFile.ts
@@ -1,19 +1,19 @@
 const checkFile = require('../../src/extensions/functions/checkFile')
 const context = require('gluegun')
 
-test('checkFile detects an existing file', async () => {
-  // get the first environment variable
-  const location = './package.json'
-  // Use checkENV to make sure it exists
+test('checkFile detects an existing file', () => {
+  // use full path! otherwise flaky testing
+  const location = __dirname + '/../../package.json'
+  // Use checkFile to make sure it exists
   expect(checkFile({location}, context)).toBeTruthy()
 })
 
-test('checkFile can fail', async () => {
-  // Use checkENV to make sure it exists
+test('checkFile can fail', () => {
+  // Use checkFile to make sure it does not exist
   expect(checkFile({location: 'DOES_NOT_EXIST'}, context)).toBeFalsy()
 })
 
-test('checkFile returns false for a file that exists', async () => {
-  // Use checkFile to make sure a known directory returns false since it's not a file
+test('checkFile returns false for a file that exists', () => {
+  // Use checkFile to make sure a known dir returns false since it's not a file
   expect(checkFile({location: './src'}, context)).toBeFalsy()
 })
