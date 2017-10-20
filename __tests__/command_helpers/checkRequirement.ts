2e1c88e78d7031a61c7204dc3bcbfa1f74fb56ed
Matthew Thompson <matt.thompson@salesloft.com>
2017-10-20 15:37:28 -0400

    Update

--- __tests__/command_helpers/checkRequirement.ts
+++ __tests__/command_helpers/checkRequirement.ts
@@ -6,26 +6,21 @@ import { tail, pipe, head, flatten, toPairs } from 'ramda';
 import checkRequirement from '../../src/extensions/functions/checkRequirement'
 import solidarityExtension from '../../src/extensions/solidarity-extension'
 
-
 // Setup checkCLI
 jest.mock('../../src/extensions/functions/checkCLI')
 const checkCLI = require('../../src/extensions/functions/checkCLI')
-checkCLI.mockImplementation(async () => "It worked!")
 
 // Setup checkDir
 jest.mock('../../src/extensions/functions/checkDir')
 const checkDir = require('../../src/extensions/functions/checkDir')
-checkDir.mockImplementation(async () => "It worked!")
 
 // Setup checkENV
 jest.mock('../../src/extensions/functions/checkENV')
 const checkENV = require('../../src/extensions/functions/checkENV')
-checkENV.mockImplementation(async () => "It worked!")
 
 // Setup checkENV
 jest.mock('../../src/extensions/functions/checkFile')
 const checkFile = require('../../src/extensions/functions/checkFile')
-checkFile.mockImplementation(async () => "It worked!")
 
 const context = require('gluegun')
 
@@ -66,38 +61,101 @@ describe('checkRequirement', () => {
   test('when an invalid rule is given', async () => {
     const result = await checkRequirement(badRule, context)
     expect(result).toEqual(['Encountered unknown rule \'knope\''])
+  })
+
+  describe('when rule: cli', () => {
+    beforeEach(() => checkCLI.mockClear());
+    
+    test('sad path', async () => {
+      checkCLI.mockImplementation(async () => "Everything is broken")
+      
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "cli", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual(["Everything is broken"])
+    })
+
+    test('happy path', async () => {
+      checkCLI.mockImplementation(async () => false)
+      
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "cli", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual([[]])
     })
   })
 
-  test('when rule: cli', async () => {
-    const rule =  toPairs({ 
-      "YARN": [{ "rule": "cli", "binary": "yarn" }] 
-    })[0]
-    const result = await checkRequirement(rule, context)
-    expect(result).toEqual(["It worked!"])
+  describe('when rule: dir', () => {
+    beforeEach(() => checkDir.mockClear());
+    
+    test('happy path', async () => {
+      checkDir.mockImplementation(async () => "It worked!")
+      
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "dir", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual([[]])
+    })
+
+    test('sad path', async () => {
+      checkDir.mockImplementation(async () => undefined)
+
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "dir", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual([[]])
+    })
   })
 
-  test('when rule: dir', async () => {
-    const rule =  toPairs({ 
-      "YARN": [{ "rule": "dir", "binary": "yarn" }] 
-    })[0]
-    const result = await checkRequirement(rule, context)
-    expect(result).toEqual([[]])
+  describe('when rule: env', () => {
+    beforeEach(() => checkENV.mockClear());    
+
+    test('happy path', async () => {
+      checkENV.mockImplementation(async () => "It worked!")
+      
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "env", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual([[]])
+    })
+    
+    test('sad path', async () => {
+      checkENV.mockImplementation(async () => undefined)
+      
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "env", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual(["'$undefined' environment variable not found"])
+    })
   })
 
-  test('when rule: env', async () => {
-    const rule =  toPairs({ 
-      "YARN": [{ "rule": "env", "binary": "yarn" }] 
-    })[0]
-    const result = await checkRequirement(rule, context)
-    expect(result).toEqual([[]])
-  })
+  describe('when rule: file', () => {
+    beforeEach(() => checkFile.mockClear());    
+    
+    test('happy path', async () => {
+      checkFile.mockImplementation(() => "It worked!")
+      
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "file", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual([[]])
+    })
 
-  test('when rule: file', async () => {
-    const rule =  toPairs({ 
-      "YARN": [{ "rule": "file", "binary": "yarn" }] 
-    })[0]
-    const result = await checkRequirement(rule, context)
-    expect(result).toEqual([[]])
+    test('sad path', async () => {
+      checkFile.mockImplementation(() => undefined)      
+
+      const rule =  toPairs({ 
+        "YARN": [{ "rule": "file", "binary": "yarn" }] 
+      })[0]
+      const result = await checkRequirement(rule, context)
+      expect(result).toEqual(["'$undefined' file not found"])
+    })
   })
 });
