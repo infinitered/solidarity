599bb0aa9500c044288022a369f6fab6ff267224
Matthew Thompson <matt.thompson@salesloft.com>
2017-10-20 14:13:23 -0400

    Improve test coverage!!

--- __tests__/command_helpers/checkRequirement.ts
+++ __tests__/command_helpers/checkRequirement.ts
@@ -6,20 +6,47 @@ import { tail, pipe, head, flatten, toPairs } from 'ramda';
 import checkRequirement from '../../src/extensions/functions/checkRequirement'
 import solidarityExtension from '../../src/extensions/solidarity-extension'
 
+
+// Setup checkCLI
+jest.mock('../../src/extensions/functions/checkCLI')
+const checkCLI = require('../../src/extensions/functions/checkCLI')
+checkCLI.mockImplementation(async () => "It worked!")
+
+// Setup checkDir
+jest.mock('../../src/extensions/functions/checkDir')
+const checkDir = require('../../src/extensions/functions/checkDir')
+checkDir.mockImplementation(async () => "It worked!")
+
+// Setup checkENV
+jest.mock('../../src/extensions/functions/checkENV')
+const checkENV = require('../../src/extensions/functions/checkENV')
+checkENV.mockImplementation(async () => "It worked!")
+
+// Setup checkENV
+jest.mock('../../src/extensions/functions/checkFile')
+const checkFile = require('../../src/extensions/functions/checkFile')
+checkFile.mockImplementation(async () => "It worked!")
+
 const context = require('gluegun')
 
-const badRule = toPairs({ 
+const badRule = toPairs({
   "YARN": [{ "rule": "knope", "binary": "yarn" }] 
 })[0]
 
+let fail
+let stop
+let succeed
+
 describe('checkRequirement', () => {
-  beforeAll(() => {
-    const fail = jest.fn()
-    const stop = jest.fn()
+  beforeEach(() => {
+    fail = jest.fn()
+    stop = jest.fn()
+    succeed = jest.fn()
     
     const spinner = {
-      fail: jest.fn(),
-      stop: jest.fn()
+      fail,
+      stop,
+      succeed
     }
 
     solidarityExtension(context)
@@ -42,19 +69,35 @@ describe('checkRequirement', () => {
     })
   })
 
-  // test('when rule: cli', () => {
+  test('when rule: cli', async () => {
+    const rule =  toPairs({ 
+      "YARN": [{ "rule": "cli", "binary": "yarn" }] 
+    })[0]
+    const result = await checkRequirement(rule, context)
+    expect(result).toEqual(["It worked!"])
+  })
 
-  // })
+  test('when rule: dir', async () => {
+    const rule =  toPairs({ 
+      "YARN": [{ "rule": "dir", "binary": "yarn" }] 
+    })[0]
+    const result = await checkRequirement(rule, context)
+    expect(result).toEqual([[]])
+  })
 
-  // test('when rule: dir', () => {
+  test('when rule: env', async () => {
+    const rule =  toPairs({ 
+      "YARN": [{ "rule": "env", "binary": "yarn" }] 
+    })[0]
+    const result = await checkRequirement(rule, context)
+    expect(result).toEqual([[]])
+  })
 
-  // })
-
-  // test('when rule: env', () => {
-
-  // })
-
-  // test('when rule: file', () => {
-
-  // })
+  test('when rule: file', async () => {
+    const rule =  toPairs({ 
+      "YARN": [{ "rule": "file", "binary": "yarn" }] 
+    })[0]
+    const result = await checkRequirement(rule, context)
+    expect(result).toEqual([[]])
+  })
 });
