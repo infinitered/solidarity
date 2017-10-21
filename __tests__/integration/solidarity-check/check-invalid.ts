1eb3deb04be5264a4079b1b0d4cef8a3d6e8e981
Bnaya Peretz <me@bnaya.net>
2017-10-21 17:00:14 +0300

    Restore CWD after test

--- __tests__/integration/solidarity-check/check-invalid.ts
+++ __tests__/integration/solidarity-check/check-invalid.ts
@@ -2,10 +2,11 @@ import execa from 'execa'
 import tempy from 'tempy'
 
 const SOLIDARITY = `${process.cwd()}/bin/solidarity`
+const origCwd = process.cwd();
 
 beforeAll(() => {
   const tempDir = tempy.directory()
-  process.chdir(tempDir)
+  process.chdir(tempDir) 
 })
 
 test('default looks for .solidarity file', async done => {
@@ -16,4 +17,8 @@ test('default looks for .solidarity file', async done => {
     expect(err.code).toBe(3)
     done()
   }
+})
+
+afterAll(() => {
+  process.chdir(origCwd) 
 })
