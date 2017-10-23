b40d4a8fbdd090557c1c327b19cee08140948924
Gant <GantMan@gmail.com>
2017-10-22 19:36:52 -0500

    test new help

--- __tests__/commands/help.ts
+++ __tests__/commands/help.ts
@@ -1,5 +1,17 @@
 import helpCommand from '../../src/commands/help'
 
+let counter = 0
+const context = {
+  print: {
+    info: () => counter++,
+    printCommands: () => counter++,
+    success: () => counter++,
+    colors: {
+      magenta: () => counter++
+    }
+  }
+}
+
 test('Snapshot check help command', () => {
   expect(helpCommand).toMatchSnapshot()
 })
@@ -8,4 +20,10 @@ test('Enforce required properties', () => {
   expect(helpCommand.description).toBeTruthy()
   expect(helpCommand.run).toBeTruthy()
   expect(typeof helpCommand.run).toBe('function')
+})
+
+test('Hash run command printing', () => {
+  expect(counter).toBe(0)
+  helpCommand.run(context)
+  expect(counter).toBe(11)
 })
