46bce68efc6d1538dbc42bac64115a477e55bfac
Gant <GantMan@gmail.com>
2017-10-23 22:58:08 -0700

    linting in CI

--- src/extensions/functions/skipRule.ts
+++ src/extensions/functions/skipRule.ts
@@ -1,7 +1,6 @@
 // Return true if we should skip
 module.exports = (platform: string | string[]): boolean => {
   if (typeof platform === 'string') {
-    // it's a string
     return platform !== process.platform
   } else if (typeof platform === 'object' && platform.length > 0) {
     // it's an array
