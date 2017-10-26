46d785031d782c5698720d9452b84451d770b4b8
Gant <GantMan@gmail.com>
2017-10-25 22:56:48 -0700

    change timout for integration tests

--- __tests__/integration/solidarity-check/check-invalid.ts
+++ __tests__/integration/solidarity-check/check-invalid.ts
@@ -3,10 +3,20 @@ import tempy from 'tempy'
 
 const SOLIDARITY = `${process.cwd()}/bin/solidarity`
 const origCwd = process.cwd()
+let originalTimeout
 
 beforeAll(() => {
+  // These can be slow on CI
+  originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000
+  // Tempy!
   const tempDir = tempy.directory()
   process.chdir(tempDir)
+})
+
+afterAll(function () {
+  // Fix timeout change
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout
 })
 
 test('default looks for .solidarity file', async done => {
