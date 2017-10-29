d8fac74ca9e9ea50bf29905766294409a94455be
Gant <GantMan@gmail.com>
2017-10-29 18:42:00 -0500

    increasing test coverage for 1.0

--- __tests__/command_helpers/updateVersions.ts
+++ __tests__/command_helpers/updateVersions.ts
@@ -1,3 +1,21 @@
 import updateVersions from '../../src/extensions/functions/updateVersions'
+const solidarity = {
+  getSolidaritySettings: jest.fn(() => ({
+    requirements: [
+      {nachos: { rule: 'cli', binary: 'tacos'}}
+    ]
+  })),
+  setSolidaritySettings: jest.fn(),
+  updateRequirement: jest.fn()
+}
+
+const mockContext = { solidarity, print: jest.fn() }
 
 test('updateVersions exists', () => expect(updateVersions).toMatchSnapshot())
+
+test('updateVersions pulls solidarity settings', () => {
+  const theVoid = updateVersions(mockContext)
+  expect(theVoid).toMatchSnapshot()
+  expect(solidarity.getSolidaritySettings.mock.calls.length).toBe(1)
+  expect(solidarity.updateRequirement.mock.calls.length).toBe(1)
+})
--- __tests__/commands/solidarity.ts
+++ __tests__/commands/solidarity.ts
@@ -1,5 +1,21 @@
 import checkCommand from '../../src/commands/solidarity'
 
+const mockContext = {
+  print: {
+    error: jest.fn(),
+    success: jest.fn()
+  },
+  parameters: {
+    options: {}
+  },
+  solidarity: {
+    checkRequirement: jest.fn(),
+    getSolidaritySettings: jest.fn(() => ({
+      config: {}
+    }))
+  }
+}
+
 test('Snapshot check default command', () => {
   expect(checkCommand).toMatchSnapshot()
 })
@@ -8,4 +24,10 @@ test('Enforce required properties', () => {
   expect(checkCommand.description).toBeTruthy()
   expect(checkCommand.run).toBeTruthy()
   expect(typeof checkCommand.run).toBe('function')
+})
+
+test('check solidarity run', async () => {
+  const result = await checkCommand.run(mockContext)
+  expect(result).toMatchSnapshot()
+  expect(mockContext.solidarity.getSolidaritySettings.mock.calls.length).toBe(1)
 })
