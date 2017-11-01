9c56ea808c4f2f2ecf910f050e63049c23277d25
Gant <GantMan@gmail.com>
2017-10-31 23:27:38 -0500

    packages and tests get facelifts

--- src/extensions/functions/setSolidaritySettings.ts
+++ src/extensions/functions/setSolidaritySettings.ts
@@ -2,15 +2,15 @@ import { SolidarityRunContext, SolidaritySettings } from '../../types'
 module.exports = (settings: SolidaritySettings, context: SolidarityRunContext): void => {
   const { print, filesystem } = context
 
-  try {
-    if (settings.requirements) {
+  if (settings.requirements) {
+    try {
       // Write file
       filesystem.write('.solidarity', JSON.stringify(settings, null, 2), {atomic: true})
-    } else {
-      throw 'You must have a requirements key to be a valid solidarity file'
+    } catch (e) {
+      print.error(e)
+      process.exit(1)
     }
-  } catch (e) {
-    print.error(e)
-    process.exit(1)
+  } else {
+    throw 'You must have a requirements key to be a valid solidarity file'
   }
 }
