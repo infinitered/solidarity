0fc255c3da3eb242526e5397421208f1f7acb80c
Gant Laborde <gantman@gmail.com>
2017-10-31 23:52:36 -0500

    packages and tests get facelifts (#112) :baby:

--- __tests__/command_helpers/setSolidaritySettings.ts
+++ __tests__/command_helpers/setSolidaritySettings.ts
@@ -1,3 +1,28 @@
 import setSolidaritySettings from '../../src/extensions/functions/setSolidaritySettings'
 
+const goodSettings = {
+  requirements: []
+}
+const badSettings = {}
+const mockContext = {
+  print: {
+    error: jest.fn()
+  },
+  filesystem: {
+    write: jest.fn()
+  }
+}
+
 test('setSolidaritySettings exists', () => expect(setSolidaritySettings).toMatchSnapshot())
+
+test('check good setSolidarity path', () => {
+  const resultVoid = setSolidaritySettings(goodSettings, mockContext)
+  expect(resultVoid).toMatchSnapshot()
+  expect(mockContext.filesystem.write.mock.calls.length).toBe(1)
+})
+
+test('check failed setSolidarity path', () => {
+  expect(() => {
+    setSolidaritySettings(badSettings, mockContext)
+  }).toThrowError('You must have a requirements key to be a valid solidarity file')
+})
