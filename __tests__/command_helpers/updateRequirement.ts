2b4fc00e9cd02b7acee53aa1b8787af35ac21799
Gant <GantMan@gmail.com>
2017-11-03 22:04:22 -0500

    slight improvement on tests

--- __tests__/command_helpers/updateRequirement.ts
+++ __tests__/command_helpers/updateRequirement.ts
@@ -1,3 +1,36 @@
 import updateRequirement from '../../src/extensions/functions/updateRequirement'
 
+const requirements = {
+  fail: [{ rule: 'cli', binary: 'tacos'}],
+  success: [{ rule: 'cli', binary: 'node', semver: '0.0.0'}]
+}
+
+const solidarity = {
+  getSolidaritySettings: jest.fn(() => ({
+    requirements
+  })),
+  setSolidaritySettings: jest.fn(),
+  updateRequirement: jest.fn()
+}
+
+const mockContext = {
+  solidarity,
+  print: {
+    spin: jest.fn(),
+    error: jest.fn()
+  }
+}
+
 test('updateRequirement exists', () => expect(updateRequirement).toMatchSnapshot())
+
+test('updateVersions pulls solidarity settings', async () => {
+  const theVoid = await updateRequirement({}, {}, mockContext)
+  expect(theVoid).toMatchSnapshot()
+  expect(mockContext.print.spin.mock.calls.length).toBe(1)
+})
+
+test('updateVersions pulls solidarity settings', async () => {
+  const theVoid = await updateRequirement(requirements.success, {}, mockContext)
+  expect(theVoid).toMatchSnapshot()
+  console.log('\n\n\n', theVoid)
+})
