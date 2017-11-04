6522ad36317f7b1c908e636f41e3e16e682c2435
Gant Laborde <gantman@gmail.com>
2017-11-03 23:30:28 -0500

    Small test bump :chart_with_upwards_trend: (#117)
    * slight improvement on tests
    
    * helping test coverage ever so slightly

--- __tests__/command_helpers/updateRequirement.ts
+++ __tests__/command_helpers/updateRequirement.ts
@@ -1,3 +1,25 @@
-import updateRequirement from '../../src/extensions/functions/updateRequirement'
+const updateRequirement = require('../../src/extensions/functions/updateRequirement')
+
+const mockContext = {
+  print: {
+    spin: jest.fn(() => ({
+      succeed: jest.fn(),
+      stop: jest.fn()
+    })),
+    error: jest.fn()
+  }
+}
 
 test('updateRequirement exists', () => expect(updateRequirement).toMatchSnapshot())
+
+test('updateRequirement empty still spins', async () => {
+  const theVoid = await updateRequirement([], {}, mockContext)
+  expect(theVoid).toMatchSnapshot()
+  expect(mockContext.print.spin.mock.calls.length).toBe(1)
+})
+
+test('updateRequirement explains missing binary', async () => {
+  const requirement = ['Failure', [{ rule: 'cli', binary: 'fakeland', semver: '0.0.0'}]]
+  const failesauce = await updateRequirement(requirement, {}, mockContext)
+  expect(failesauce).toEqual([ 'Binary \'fakeland\' not found' ])
+})
