d0c7b95c2cd6cf7af3d2880c297916eb29f3804b
Matthew Thompson <matt.thompson@salesloft.com>
2017-11-04 22:49:49 -0400

    Initial layout of a new spec that is not working correctly.

--- __tests__/command_helpers/updateRequirement.ts
+++ __tests__/command_helpers/updateRequirement.ts
@@ -1,3 +1,39 @@
+import { platform } from 'os';
+import { toPairs } from 'ramda'
 import updateRequirement from '../../src/extensions/functions/updateRequirement'
+import solidarityExtension from '../../src/extensions/solidarity-extension'
+
+const context = require('gluegun')
+// jest.mock('../../src/functions/skipRule')
+// const skipRule = require('../../src/functions/skipRule')
+
+// Seems weird that this is not used from what I can tell
+const settings = {}
 
 test('updateRequirement exists', () => expect(updateRequirement).toMatchSnapshot())
+
+describe('updateRequirement', () => {
+  beforeAll(() => {
+    context.print = {
+      spin: jest.fn(),
+      error: jest.fn(),
+    }
+  })
+
+  describe('given skipRule returns true', () => {
+    test('it returns an empty []', async () => {
+
+      const requirement = toPairs({
+        Yarn: [{
+          "rule": "cli",
+          "binary": "yarn",
+          "version": "--version"
+        }]
+      })[0]
+
+
+      const result = await updateRequirement(requirement, settings, context)
+      expect(result).toEqual([])
+    })
+  })
+})
