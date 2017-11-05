f2724e926c9ad323ad855272184e14eecea4eaee
Matthew Thompson <matt.thompson@salesloft.com>
2017-11-04 23:56:35 -0400

    Last test for updateRequirement.ts

--- __tests__/command_helpers/updateRequirement.ts
+++ __tests__/command_helpers/updateRequirement.ts
@@ -1,20 +1,20 @@
 import { platform } from 'os';
 import { toPairs } from 'ramda'
 import updateRequirement from '../../src/extensions/functions/updateRequirement'
-import solidarityExtension from '../../src/extensions/solidarity-extension'
 
 const context = require('gluegun')
-// jest.mock('../../src/functions/skipRule')
-// const skipRule = require('../../src/functions/skipRule')
+jest.mock('../../src/extensions/functions/checkCLIForUpdates')
+const checkCLIForUpdates = require('../../src/extensions/functions/checkCLIForUpdates')
 
 // Seems weird that this is not used from what I can tell
 const settings = {}
+let spinner
 
 test('updateRequirement exists', () => expect(updateRequirement).toMatchSnapshot())
 
 describe('updateRequirement', () => {
-  beforeAll(() => {
-    const spinner = {
+  beforeEach(() => {
+    spinner = {
       succeed: jest.fn(),
       stop: jest.fn()
     }
@@ -26,7 +26,7 @@ describe('updateRequirement', () => {
   })
 
   describe('given skipRule returns true', () => {
-    test('it returns an empty []', async () => {
+    it('returns an empty []', async () => {
       const requirement = toPairs({
         Yarn: [{
           "rule": "cli",
@@ -43,17 +43,82 @@ describe('updateRequirement', () => {
   })
 
   describe('without a semver returns', () => {
-    test('it returns an empty []', async () => {
+    it('returns an empty []', async () => {
       const requirement = toPairs({
         Yarn: [{
           "rule": "cli",
           "binary": "yarn",
-          "version": "--version",
+          "version": "--version"
         }]
       })[0]
 
       const result = await updateRequirement(requirement, settings, context)
       expect(result).toEqual([[]])
+    })
+  })
+
+  describe('given a rule', () => {
+    describe('rule: cli', () => {
+      afterEach(() => {
+        checkCLIForUpdates.mockClear()
+      })
+
+      describe('when checkCLIForUpdates => true', () => {
+        beforeEach(() => {
+          checkCLIForUpdates.mockImplementation(() => Promise.resolve("Updated"))
+        })
+
+        it('returns the updateResults', async () => {
+          const requirement = toPairs({
+            Yarn: [{
+              "rule": "cli",
+              "binary": "yarn",
+              "version": "--version",
+              "semver": "1.1.0"
+            }]
+          })[0]
+
+          const result = await updateRequirement(requirement, settings, context)
+          expect(result).toEqual(["Updated"])
+          expect(spinner.succeed.mock.calls).toEqual([["Updated"]])
+        })
+      })
+
+      describe('when checkCLIForUpdates => false', () => {
+        beforeEach(() => {
+          checkCLIForUpdates.mockImplementation(() => false)
+        })
+
+        it("should return an empty array", async () => {
+          const requirement = toPairs({
+            Yarn: [{
+              "rule": "cli",
+              "binary": "yarn",
+              "version": "--version",
+              "semver": "1.1.0"
+            }]
+          })[0]
+
+          const result = await updateRequirement(requirement, settings, context)
+          expect(result).toEqual([[]])
+          expect(spinner.succeed.mock.calls).toEqual([['Keep yarn 1.1.0']])
+        })
+      })
+    })
+
+    describe('rule: !cli', () => {
+      it('returns an empty array', async () => {
+        const requirement = toPairs({
+          Yarn: [{
+            "rule": "dir",
+            "location": "./src",
+          }]
+        })[0]
+
+        const result = await updateRequirement(requirement, settings, context)
+        expect(result).toEqual([[]])
+        expect(spinner.stop.mock.calls.length).toEqual(1)
+      })
     })
   })
 })
