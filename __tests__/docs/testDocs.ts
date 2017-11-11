482b033318e565f6df16f341dfd313954bca2095
Gant <GantMan@gmail.com>
2017-11-11 13:03:36 -0600

    platform independent path sep

--- __tests__/docs/testDocs.ts
+++ __tests__/docs/testDocs.ts
@@ -1,8 +1,9 @@
 import jetpack from 'fs-jetpack'
+import path from 'path'
 
 test('Verify each markdown file has a link in sidebar', () => {
   const markdownFiles = jetpack.find('docs', { matching: ['*.md', '!_*.md'] })
-  const sidebarContents = jetpack.read('docs/_sidebar.md')
+  const sidebarContents = jetpack.read(`docs${path.sep}_sidebar.md`)
   markdownFiles.map((fileName) => {
     expect(sidebarContents.includes(fileName)).toBe(true)
   })
