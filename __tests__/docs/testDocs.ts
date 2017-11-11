bec6e43e0b7d79ea9f4cbc76862404e3ee000d32
Kevin VanGelder <kevin@infinite.red>
2017-11-11 11:39:44 -0800

    fix separator issue

--- __tests__/docs/testDocs.ts
+++ __tests__/docs/testDocs.ts
@@ -5,6 +5,6 @@ test('Verify each markdown file has a link in sidebar', () => {
   const markdownFiles = jetpack.find('docs', { matching: ['*.md', '!_*.md'] })
   const sidebarContents = jetpack.read(`docs${path.sep}_sidebar.md`)
   markdownFiles.map((fileName) => {
-    expect(sidebarContents.includes(fileName)).toBe(true)
+    expect(sidebarContents.includes(fileName.split(path.sep).join('/')).toBe(true)
   })
 })
