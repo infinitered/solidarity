01db7c4249ea965a8a10313f8bd76f236678173f
Gant <GantMan@gmail.com>
2017-11-19 22:58:21 -0600

    check file variations

--- __tests__/command_helpers/getSolidaritySettings.ts
+++ __tests__/command_helpers/getSolidaritySettings.ts
@@ -4,16 +4,24 @@ import getSolidaritySettings from '../../src/extensions/functions/getSolidarityS
 const context = require('gluegun')
 
 describe('getSolidaritySettings', () => {
-  describe('w/ .solidarity', () => {
+  describe('w/ success', () => {
     test('getSolidaritySettings exists', () => expect(getSolidaritySettings).toMatchSnapshot())
-    
+
     test('getSolidaritySettings succeeds', async () => {
       const resultSettings = getSolidaritySettings(context)
       // we got an object with requirements defined
       expect(resultSettings).toMatchObject({requirements: {}})
     })
+
+    test('getSolidaritySettings succeeds', async () => {
+      process.chdir('__tests__/sandbox/solidarity_json')
+      const resultSettings = getSolidaritySettings(context)
+      // we got an object with requirements defined
+      expect(resultSettings).toMatchObject({requirements: {}})
+      process.chdir('../../../')
+    })
   })
-  
+
   test('getSolidaritySettings can fail', async () => {
     expect(() => {
       process.chdir('__tests__')
@@ -21,5 +29,12 @@ describe('getSolidaritySettings', () => {
     }).toThrow()
     process.chdir('../')
   })
-})
 
+  test('getSolidaritySettings can warn with missing requirements', async () => {
+    expect(() => {
+      process.chdir('__tests__/sandbox/solidarity_broken')
+      const resultSettings = getSolidaritySettings(context)
+    }).toThrowError('ERROR: Found, but no requirements key.  Please validate your solidarity file')
+    process.chdir('../../../')
+  })
+})
