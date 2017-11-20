803b33e310bb60e443dbf6df246f133a712487fd
Gant <GantMan@gmail.com>
2017-11-19 23:06:52 -0600

    return the namespacing

--- src/commands/solidarity.ts
+++ src/commands/solidarity.ts
@@ -1,90 +1,92 @@
 import { GluegunCommand } from 'gluegun'
 import { SolidarityOutputMode, SolidaritySettings } from '../types'
 
-const { map, toPairs, isEmpty, flatten, reject, isNil } = require('ramda')
+namespace Solidarity {
+  const { map, toPairs, isEmpty, flatten, reject, isNil } = require('ramda')
 
-const checkForEscapeHatchFlags = async (context) => {
-  const { print, parameters } = context
-  const { options } = parameters
-  if (options.help || options.h) {
-    // Just looking for help
-    print.printCommands(context)
-    process.exit(0)
-  } else if (options.version || options.v) {
-    // Just looking for version
-    print.info(require('../../package.json').version)
-    process.exit(0)
-  }
-}
-
-const setOutputMode = (parameters, settings: SolidaritySettings): SolidarityOutputMode => {
-  const { options } = parameters
-  // CLI flags override config
-  if (options.verbose || options.a) {
-    return SolidarityOutputMode.VERBOSE
-  } else if (options.silent || options.s) {
-    return SolidarityOutputMode.SILENT
-  } else if (options.moderate || options.m) {
-    return SolidarityOutputMode.MODERATE
+  const checkForEscapeHatchFlags = async (context) => {
+    const { print, parameters } = context
+    const { options } = parameters
+    if (options.help || options.h) {
+      // Just looking for help
+      print.printCommands(context)
+      process.exit(0)
+    } else if (options.version || options.v) {
+      // Just looking for version
+      print.info(require('../../package.json').version)
+      process.exit(0)
+    }
   }
 
-  // Set output mode, set to default on invalid value
-  let outputModeString = settings.config ? String(settings.config.output).toUpperCase() : 'MODERATE'
-  return SolidarityOutputMode[outputModeString] || SolidarityOutputMode.MODERATE
-}
+  const setOutputMode = (parameters, settings: SolidaritySettings): SolidarityOutputMode => {
+    const { options } = parameters
+    // CLI flags override config
+    if (options.verbose || options.a) {
+      return SolidarityOutputMode.VERBOSE
+    } else if (options.silent || options.s) {
+      return SolidarityOutputMode.SILENT
+    } else if (options.moderate || options.m) {
+      return SolidarityOutputMode.MODERATE
+    }
 
-export const run = async (context) => {
-  // drop out fast in these situations
-  await checkForEscapeHatchFlags(context)
+    // Set output mode, set to default on invalid value
+    let outputModeString = settings.config ? String(settings.config.output).toUpperCase() : 'MODERATE'
+    return SolidarityOutputMode[outputModeString] || SolidarityOutputMode.MODERATE
+  }
 
-  const { print, solidarity } = context
-  const { checkRequirement, getSolidaritySettings } = solidarity
+  export const run = async (context) => {
+    // drop out fast in these situations
+    await checkForEscapeHatchFlags(context)
 
-  // get settings or error
-  let solidaritySettings
-  try {
-    solidaritySettings = getSolidaritySettings(context)
-  } catch (e) {
-    print.error(e)
-    print.info(
-      `Make sure you are in the correct folder or run ${print.colors.success(
-        'solidarity snapshot'
-      )} to take a snapshot of your environment and create a .solidarity file for this project.`
+    const { print, solidarity } = context
+    const { checkRequirement, getSolidaritySettings } = solidarity
+
+    // get settings or error
+    let solidaritySettings
+    try {
+      solidaritySettings = getSolidaritySettings(context)
+    } catch (e) {
+      print.error(e)
+      print.info(
+        `Make sure you are in the correct folder or run ${print.colors.success(
+          'solidarity snapshot'
+        )} to take a snapshot of your environment and create a .solidarity file for this project.`
+      )
+      process.exit(3)
+    }
+
+    context.outputMode = setOutputMode(context.parameters, solidaritySettings)
+
+    // build map of checks to perform
+    const checks = await map(
+      async requirement => checkRequirement(requirement, context),
+      toPairs(solidaritySettings.requirements)
     )
-    process.exit(3)
+
+    // run the array of promises you just created
+    await Promise.all(checks)
+      .then(results => {
+        const errors = reject(isNil, flatten(results))
+        const silentOutput = context.outputMode === SolidarityOutputMode.SILENT
+        // Add empty line between final result if printing rule results
+        if (!silentOutput) print.success('')
+
+        if (isEmpty(errors)) {
+          if (!silentOutput) print.success(print.checkmark + ' Solidarity checks valid')
+        } else {
+          if (!silentOutput) print.error('Solidarity checks failed.')
+          process.exit(1)
+        }
+      })
+      .catch(err => {
+        print.error(err)
+        process.exit(2)
+      })
   }
-
-  context.outputMode = setOutputMode(context.parameters, solidaritySettings)
-
-  // build map of checks to perform
-  const checks = await map(
-    async requirement => checkRequirement(requirement, context),
-    toPairs(solidaritySettings.requirements)
-  )
-
-  // run the array of promises you just created
-  await Promise.all(checks)
-    .then(results => {
-      const errors = reject(isNil, flatten(results))
-      const silentOutput = context.outputMode === SolidarityOutputMode.SILENT
-      // Add empty line between final result if printing rule results
-      if (!silentOutput) print.success('')
-
-      if (isEmpty(errors)) {
-        if (!silentOutput) print.success(print.checkmark + ' Solidarity checks valid')
-      } else {
-        if (!silentOutput) print.error('Solidarity checks failed.')
-        process.exit(1)
-      }
-    })
-    .catch(err => {
-      print.error(err)
-      process.exit(2)
-    })
 }
 
 // Export command
 module.exports = {
   description: 'Check environment against solidarity rules',
-  run
+  run: Solidarity.run
 } as GluegunCommand
