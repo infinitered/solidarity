4a9dea703f7c3ce33ac99c92f42b17ab7d35f31a
Gant <GantMan@gmail.com>
2017-11-27 22:57:16 -0600

    all tested up

--- __tests__/command_helpers/printResults.ts
+++ __tests__/command_helpers/printResults.ts
@@ -1,5 +1,33 @@
 import printResults from '../../src/extensions/functions/printResults'
+import { SolidarityRunContext, SolidarityReportResults } from '../../src/types'
 
-test('printResults does things', () => {
-  // expect(await checkENV({})).toBeFalsy()
+let mockContext: SolidarityRunContext
+let reportResults: SolidarityReportResults
+describe('reviewRule', () => {
+  beforeEach(() => {
+    // fresh mock context
+    mockContext = require('mockContext')
+    reportResults = {
+      basicInfo: [
+        ['System Basics', 'Value'],
+        ['OS', 'tacoOS'],
+        ['CPU', 'hamsters in a wheel']
+      ],
+      cliRules: [
+        ['Binary', 'Location', 'Version', 'Desired']
+      ],
+      envRules: [
+        ['Environment Var', 'Value']
+      ],
+      filesystemRules: [
+        ['Location', 'Type', 'Exists']
+      ]
+    }
+  })
+
+  test('printResults uses table' async () => {
+    const result = await printResults(reportResults, mockContext)
+    // print table was called
+    expect(mockContext.print.table).toHaveBeenCalled()
+  })
 })
