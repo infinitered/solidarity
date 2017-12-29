6bdac0bba845a42612b4aa55ae67a311362907bc
Gant <GantMan@gmail.com>
2017-12-28 22:46:12 -0600

    playing with fire

--- dangerfile.js
+++ dangerfile.js
@@ -1,17 +1,15 @@
-import { message, danger, warn, includes } from "danger"
+import { message, danger, warn } from 'danger'
 message(":tada:, this worked @" + danger.github.pr.user.login)
-// import { warn } from 'danger'
 // import spellcheck from 'danger-plugin-spellcheck'
-// warn('hellow world')
 
 // let's spellcheck
 // schedule(spellcheck())
 // spellcheck()
 
 // Enforce yarn.lock updates
-const packageChanged = includes(danger.git.modified_files, 'package.json')
-const yarnLockfileChanged = includes(danger.git.modified_files, 'yarn.lock')
-const npmLockfileChanged = includes(danger.git.modified_files, 'package-lock.json')
+const packageChanged = danger.git.modified_files.includes('package.json')
+const yarnLockfileChanged = danger.git.modified_files.includes('yarn.lock')
+const npmLockfileChanged = danger.git.modified_files.includes('package-lock.json')
 if (packageChanged && !yarnLockfileChanged) {
   const message = 'Changes were made to package.json, but not to yarn.lock'
   const idea = 'Perhaps you need to run `yarn install`?'
