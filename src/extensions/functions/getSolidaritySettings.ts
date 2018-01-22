cecd47e6c155964ae186c8202e7204d7604c3ff0
Gant Laborde <gantman@gmail.com>
2018-01-21 22:32:55 -0600

    :dizzy: Support JSON5 (#165)
    * add docs update
    
    * support reading JSON5
    
    * update docs on jason5

--- src/extensions/functions/getSolidaritySettings.ts
+++ src/extensions/functions/getSolidaritySettings.ts
@@ -1,13 +1,14 @@
 import { SolidarityRunContext, SolidaritySettings } from '../../types'
+import * as JSON5 from 'json5'
 module.exports = (context: SolidarityRunContext): SolidaritySettings => {
   const { filesystem } = context
 
-  // for now only JSON support
+  // for now only JSON and JSON5 support
   let solidaritySettings
   if (filesystem.exists('.solidarity')) {
-    solidaritySettings = JSON.parse(filesystem.read('.solidarity'))
+    solidaritySettings = JSON5.parse(filesystem.read('.solidarity'))
   } else if (filesystem.exists('.solidarity.json')) {
-    solidaritySettings = JSON.parse(filesystem.read('.solidarity.json'))
+    solidaritySettings = JSON5.parse(filesystem.read('.solidarity.json'))
   } else {
     // if we got here there was no solidarity file
     throw 'ERROR: No solidarity file was found'
