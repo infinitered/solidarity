5321e3bd8763a95abe8f40fbec2ba3ae470b175b
Gant <GantMan@gmail.com>
2018-02-03 17:42:13 -0600

    :construction: customized reports on existing reports

--- __tests__/command_helpers/solidarityReport.ts
+++ __tests__/command_helpers/solidarityReport.ts
@@ -1,10 +1,11 @@
 import { SolidarityReportResults } from '../../src/types'
 import { createReport } from '../../src/extensions/functions/solidarityReport'
+const context = require('mockContext')
 
 let reportResults: SolidarityReportResults
 describe('solidarityReport structure', () => {
   test('the basic function generates the Result object', () => {
-    let report = createReport()
+    let report = createReport(context)
     expect(report).toMatchSnapshot()
   })
 })
--- src/extensions/functions/solidarityReport.ts
+++ src/extensions/functions/solidarityReport.ts
@@ -1,12 +1,26 @@
-import { SolidarityReportResults } from '../../types'
+import { SolidarityRunContext, SolidarityReportResults, CLIReportConfig } from '../../types'
 import { helpers } from 'envinfo'
 
-export const createReport = (): SolidarityReportResults => {
+export const createReport = (context: SolidarityRunContext): SolidarityReportResults => {
+  const { print, system } = context
+  const { colors } = print
+
   return {
     basicInfo: [['System Basics', 'Value'], ['OS', helpers.getOperatingSystemInfo()], ['CPU', helpers.getCPUInfo()]],
     cliRules: [['Binary', 'Location', 'Version', 'Desired']],
     envRules: [['Environment Var', 'Value']],
     filesystemRules: [['Location', 'Type', 'Exists']],
     shellRules: [['Command', 'Pattern', 'Matches']],
+    // helper for adding CLI rules
+    addCLI: function (cliReportConfig: CLIReportConfig) {
+      const desired = cliReportConfig.desired ? cliReportConfig.desired : colors.green('*ANY*')
+      let location
+      try {
+        location = system.which(cliReportConfig.binary)
+      } catch (_e) {
+        location = colors.red('*MISSING*')
+      }
+      this.cliRules.push([cliReportConfig.binary, location, cliReportConfig.version, desired])
+    },
   }
 }
