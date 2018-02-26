d710c968bb9f79f1b010e594535cd455f8f74da0
Gant Laborde <GantMan@gmail.com>
2018-02-26 12:46:07 -0600

    fix for wind0ze

--- src/extensions/functions/quirksNodeModules.ts
+++ src/extensions/functions/quirksNodeModules.ts
@@ -2,9 +2,10 @@ import {
   reject, contains, concat, difference
 } from 'ramda'
 import path from 'path'
+const delineator = process.platform === 'win32' ? ';' : ':'
 
 // Node mutates path by adding to the front, move that to the back if it exists
 const originalPath = process.env.PATH || ''
-const originalArray = originalPath.split(':')
+const originalArray = originalPath.split(delineator)
 const cleanArray = reject(contains('node_modules' + path.sep), originalArray)
-process.env.PATH = concat(cleanArray, difference(originalArray, cleanArray)).join(':')
+process.env.PATH = concat(cleanArray, difference(originalArray, cleanArray)).join(delineator)
