cdffb8eb1be0e55aee5c5ec5eff03b4423731197
Gant <GantMan@gmail.com>
2018-02-26 10:06:51 -0600

    prefer const over let

--- src/extensions/functions/quirksNodeModules.ts
+++ src/extensions/functions/quirksNodeModules.ts
@@ -4,7 +4,7 @@ import {
 import path from 'path'
 
 // Node mutates path by adding to the front, move that to the back if it exists
-let originalPath = process.env.PATH || ''
-let originalArray = originalPath.split(':')
-let cleanArray = reject(contains('node_modules' + path.sep), originalArray)
+const originalPath = process.env.PATH || ''
+const originalArray = originalPath.split(':')
+const cleanArray = reject(contains('node_modules' + path.sep), originalArray)
 process.env.PATH = concat(cleanArray, difference(originalArray, cleanArray)).join(':')
