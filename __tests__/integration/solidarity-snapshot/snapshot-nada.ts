7141494b16104aaf57c4ffc8567ce5f1256836d7
Gant <GantMan@gmail.com>
2018-03-01 21:52:49 -0600

    testing empty snapshot

---
+++ __tests__/integration/solidarity-snapshot/snapshot-nada.ts
@@ -0,0 +1,36 @@
+import execa from 'execa'
+import tempy from 'tempy'
+
+const SOLIDARITY = `${process.cwd()}/bin/solidarity`
+const origCwd = process.cwd()
+let originalTimeout
+
+beforeAll(() => {
+  // These can be slow on CI
+  originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 90000
+  const tempDir = tempy.directory()
+  process.chdir(tempDir)
+})
+
+afterAll(function() {
+  // Fix timeout change
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout
+})
+
+test('solidarity report works', async done => {
+  try {
+    execa.shell(`echo n | ${SOLIDARITY} snapshot`).then(result => {
+      // check a few from the report
+      expect(result).toMatchSnapshot()
+      expect(result.code).toBe(0)
+      done()
+    })
+  } catch (err) {
+    done.fail()
+  }
+})
+
+afterEach(() => {
+  process.chdir(origCwd)
+})
