3bca7ad4eaba461d98e49626a850a2300654e56b
Gant Laborde <GantMan@gmail.com>
2018-03-02 10:33:08 -0600

    stay in temp dir for test

--- __tests__/integration/solidarity-check/check-valid.ts
+++ __tests__/integration/solidarity-check/check-valid.ts
@@ -47,27 +47,29 @@ test('also looks for .solidarity.json file', async done => {
 })
 
 test('verbose flag works', async done => {
-  execa.shellSync('node --version')
-  execa.shellSync('npm --version')
+  const tempDir = tempy.directory()
+  filesystem.copy(`__tests__${path.sep}sandbox${path.sep}solidarity_json${path.sep}.solidarity.json`, `${tempDir}${path.sep}.solidarity`)
+  process.chdir(tempDir)
   try {
     await execa.shell(`${SOLIDARITY} --verbose`).then(result => {
       expect(result.stdout).toMatchSnapshot()
       done()
     })
   } catch (err) {
-    console.error('Failed Verbose:', err)
     done.fail()
   }
 })
 
 test('silent flag works', async done => {
+  const tempDir = tempy.directory()
+  filesystem.copy(`__tests__${path.sep}sandbox${path.sep}solidarity_json${path.sep}.solidarity.json`, `${tempDir}${path.sep}.solidarity`)
+  process.chdir(tempDir)
   try {
     await execa.shell(`${SOLIDARITY} --silent`).then(result => {
       expect(result.stdout).toMatchSnapshot()
       done()
     })
   } catch (err) {
-    console.error('Failed Silent:', err)
     done.fail()
   }
 })
