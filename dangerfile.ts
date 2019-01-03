5e530e3cbaa82352c0e5f1bb0f03addff56afc41
Gant Laborde <GantMan@gmail.com>
2019-01-03 16:36:09 -0600

    undo package-lock.json warning

--- dangerfile.ts
+++ dangerfile.ts
@@ -22,9 +22,9 @@ if (packageChanged && !yarnLockfileChanged) {
   const idea = 'Perhaps you need to run `yarn install`?'
   warn(`${message} - <i>${idea}</i>`)
 }
-// Enforce package-lock.json
-if (packageChanged && !npmLockfileChanged) {
-  const message = 'Changes were made to package.json, but not to package-lock.json'
-  const idea = 'Perhaps you need to run `npm install`?'
-  warn(`${message} - <i>${idea}</i>`)
-}
+// // Enforce package-lock.json
+// if (packageChanged && !npmLockfileChanged) {
+//   const message = 'Changes were made to package.json, but not to package-lock.json'
+//   const idea = 'Perhaps you need to run `npm install`?'
+//   warn(`${message} - <i>${idea}</i>`)
+// }
