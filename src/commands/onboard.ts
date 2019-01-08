26f6a2033e9ad630d7d00a920add0112e2db1984
Gant <GantMan@gmail.com>
2019-01-08 08:21:12 -0800

    starting step

---
+++ src/commands/onboard.ts
@@ -0,0 +1,34 @@
+import { GluegunCommand, GluegunRunContext } from 'gluegun'
+import { filter, propEq, head } from 'ramda'
+
+import { FriendlyMessages, SolidarityRunContext } from '../types'
+
+namespace Snapshot {
+  export const run = async (context: SolidarityRunContext) => {
+    const { print, prompt, filesystem } = context
+
+    // check is there an existing .solidarity file?
+    if (filesystem.exists('.solidarity')) {
+      print.info('you have a solidarity file')
+    } else {
+      // Find out what they wanted
+      const userAnswer = await prompt.ask({
+        name: 'createFile',
+        type: 'confirm',
+        message: 'No `.solidarity` file found for this project.  Would you like to create one?',
+      })
+
+      if (userAnswer.createFile) {
+        print.info('create wut?')
+      } else {
+        print.info('No Solidarity File')
+      }
+    }
+  }
+}
+
+module.exports = {
+  description: 'Wizard walkthrough to create your Solidarity file',
+  alias: 'o',
+  run: Snapshot.run,
+} as GluegunCommand
