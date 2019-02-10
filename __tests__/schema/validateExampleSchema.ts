27b06280c5cfd14d6966e7783455df0efa4d5cc1
Gant <GantMan@gmail.com>
2019-02-10 16:41:41 -0600

    verify existence of schema

--- __tests__/schema/validateExampleSchema.ts
+++ __tests__/schema/validateExampleSchema.ts
@@ -3,6 +3,7 @@ import Ajv from 'ajv'
 test('solidarity.example.json fits local schema', () => {
   const ajv = new Ajv()
   const localSchema = require('../../solidaritySchema.json')
+  expect(localSchema).toBeTruthy()
 
   const solidarityExample = require('../../.solidarity.example.json')
   const valid = ajv.validate(localSchema, solidarityExample)
