2cbd7ce22ffbacbcde222eebe9dded34afaadb60
Piotr Kowalski <piecioshka@gmail.com>
2019-05-01 21:47:00 +0200

    Support ~ in path with resolve-dir

--- src/extensions/functions/checkFile.ts
+++ src/extensions/functions/checkFile.ts
@@ -1,10 +1,15 @@
 import { FSRule, SolidarityRunContext } from '../../types'
 import * as path from 'path'
+
+const resolve = require('resolve-dir')
+
 module.exports = (rule: FSRule, context: SolidarityRunContext): void => {
   const { filesystem } = context
   if (rule.location) {
-    const filePath = path.isAbsolute(rule.location) ? rule.location : path.join(process.cwd(), rule.location)
-    if (filesystem.exists(filePath) !== 'file') throw new Error(`Location '${rule.location}' is not a file`)
+    const filePath = path.isAbsolute(rule.location) ? rule.location : resolve(rule.location)
+    if (filesystem.exists(filePath) !== 'file') {
+      throw new Error(`Location '${rule.location}' is not a file`)
+    }
   } else {
     throw new Error(`No location for file rule`)
   }
