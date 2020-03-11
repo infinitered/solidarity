5f0768d4bbfa84f4a91c40adfb92d0915dbdc968
Kevin VanGelder <kevin@infinite.red>
2020-03-11 13:08:35 -0700

    remove commented lines

--- src/extensions/functions/onboard/kickoffs/kickoffCLI.ts
+++ src/extensions/functions/onboard/kickoffs/kickoffCLI.ts
@@ -1,7 +1,5 @@
 import { SolidarityRunContext, SolidarityRule } from '../../../../types'
 
 export default async (context: SolidarityRunContext): Promise<SolidarityRule> => {
-  // const { print } = context
-  // print.info(`kickoff CLI function`)
   return { rule: 'cli', binary: '' }
 }
--- src/extensions/functions/onboard/kickoffs/kickoffShell.ts
+++ src/extensions/functions/onboard/kickoffs/kickoffShell.ts
@@ -1,7 +1,5 @@
 import { SolidarityRunContext, SolidarityRule } from '../../../../types'
 
 export default async (context: SolidarityRunContext): Promise<SolidarityRule> => {
-  // const { print } = context
-  // print.info(`kickoff Shell function`)
   return { rule: 'shell', command: '', match: '' }
 }
