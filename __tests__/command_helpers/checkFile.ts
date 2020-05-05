7d36c90dafafe2a27b29f411dc85f9ca129a678b
Piotr Kowalski <piecioshka@gmail.com>
2020-05-05 21:10:04 +0200

    Support ~ in path with resolve-dir (#238)
    * Support ~ in path with resolve-dir
    
    * Update deps
    
    Co-authored-by: Mark Rickert <mjar81@gmail.com>

--- __tests__/command_helpers/checkFile.ts
+++ __tests__/command_helpers/checkFile.ts
@@ -1,29 +1,44 @@
 import checkFile from '../../src/extensions/functions/checkFile'
 import * as context from 'gluegun/toolbox'
+import { FSRule } from '../../src/types'
 
 test('checkFile detects an existing file', () => {
   // known file
   const location = 'package.json'
   // Use checkFile to make sure it exists
-  expect(checkFile({ location }, context)).toBe(undefined)
+  expect(checkFile({ location } as FSRule, context)).toBe(undefined)
 })
 
 test('checkFile can fail', () => {
   // Use checkFile to make sure it does not exist
   expect(() => {
-    checkFile({ location: 'DOES_NOT_EXIST' }, context)
-  }).toThrow()
-})
-
-test('checkFile does not throw for a dir that exists', () => {
-  // Use checkFile to make sure a known dir returns false since it's not a file
-  expect(() => {
-    checkFile({ location: './src' }, context)
+    checkFile({ location: 'DOES_NOT_EXIST' } as FSRule, context)
   }).toThrow()
 })
 
 test('checkFile throws false with no location', () => {
   expect(() => {
-    checkFile({}, context)
+    checkFile({} as FSRule, context)
+  }).toThrow()
+})
+
+test('checkFile does not throw for a local dir that exists', () => {
+  // Use checkFile to make sure a known dir returns false since it's not a file
+  expect(() => {
+    checkFile({ location: './src' } as FSRule, context)
+  }).toThrow()
+})
+
+test('checkFile does not throw for a local dir (in default) that exists', () => {
+  // Use checkFile to make sure a known dir returns false since it's not a file
+  expect(() => {
+    checkFile({ location: 'src' } as FSRule, context)
+  }).toThrow()
+})
+
+test('checkFile does not throw for a homedir that exists', () => {
+  // Use checkFile to make sure a known dir returns false since it's not a file
+  expect(() => {
+    checkFile({ location: '~/.ssh/' } as FSRule, context)
   }).toThrow()
 })
--- src/extensions/functions/checkFile.ts
+++ src/extensions/functions/checkFile.ts
@@ -1,9 +1,12 @@
 import { FSRule, SolidarityRunContext } from '../../types'
 import * as path from 'path'
+
+const resolve = require('resolve-dir')
+
 module.exports = (rule: FSRule, context: SolidarityRunContext): void => {
   const { filesystem } = context
   if (rule.location) {
-    const filePath = path.isAbsolute(rule.location) ? rule.location : path.join(process.cwd(), rule.location)
+    const filePath = path.isAbsolute(rule.location) ? rule.location : resolve(rule.location)
     if (filesystem.exists(filePath) !== 'file') {
       throw new Error(rule.error || `Location '${rule.location}' is not a file`)
     }
